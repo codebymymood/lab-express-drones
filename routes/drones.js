1eb77799a46f5351b2747ff771dbbb4c6735d5d1
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-13 15:26:07 +0000

    drones-lab

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,81 @@
 const express = require('express');
 const router = express.Router();
-
+const DroneModel = require('../models/Drone.model')
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  
+    DroneModel.find()
+    .then((drone) => {
+      res.render('drones/list.hbs', {drone})
+    })
+    .catch(() => {
+     next('No list of drones available')
+    })
+
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+    res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  
+  const {name, propellers, maxSpeed} = req.body
+
+  DroneModel.create({name, propellers, maxSpeed})
+        .then(() => {
+            
+            res.redirect('/drones/')
+        })
+        .catch(() => {
+            next("Unable to create new drone")
+        })
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  
+  const {id} = req.params
+
+    DroneModel.findById(id)
+        .then((drone) => {
+          
+            res.render('drones/update-form.hbs', {drone})
+            //res.status(404).render("not-found");
+
+        })
+        .catch(() => {
+            next('Cannot find requested drone')
+        })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+  const {id} = req.params
+  
+
+  DroneModel.findByIdAndUpdate(id, {name, propellers, maxSpeed})
+        .then(() => {
+            
+            res.redirect('/drones/')
+        })
+        .catch(() => {
+            next("Unable to update required drone")
+        })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const {id} = req.params
+
+  DroneModel.findByIdAndRemove(id)
+  .then((drone) => {
+      //then send the user to the home page
+      res.redirect('/drone/', {drone})
+  })
+  .catch(() => {
+      next('Not able to delete drone')
+  })
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
-// Iteration #1
+require('../db') 
+const mongoose = require('mongoose')
+
+let DroneModel = require('../models/Drone.model')
+
+DroneModel.create([
+    { name: "Trololo", propellers: 6, maxSpeed: 25},
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+   
+])
+    .then(() => {
+        console.log('All drones were created')
+        mongoose.disconnect()
+    })
+    .catch((err) => {
+        console.log('Error', err)
+        mongoose.disconnect()
+    })
